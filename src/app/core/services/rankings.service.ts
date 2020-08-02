25874f1d9a110da4aa2fb5c643ca609a556f9fa8
ymougenel <yann.mougenel@gmail.com>
2020-08-02 13:04:25 +0200

    feat[RT]: slice rankings

--- src/app/core/services/rankings.service.ts
+++ src/app/core/services/rankings.service.ts
@@ -21,13 +21,19 @@ export class RankingsService {
   getRankingsBetween(gameId: string, startDate: Date, endDate: Date): Observable<Ranking[]> {
     let startTimeStamp = this.toTimestamp(startDate);
     let endTimeStamp = this.toTimestamp(endDate);
-    return this._http.get<Ranking[]>(`${this._config.baseUrl}/ranking/${gameId}/${startTimeStamp}/${endTimeStamp}`);
+    return this._http.get<Ranking[]>(`${this._config.baseUrl}/ranking/${gameId}/${startTimeStamp}/${endTimeStamp}`)
   }
 
   toTimestamp(date: Date): number {
     return Math.round(date.getTime() / 1000);
   }
 
+  private _sliceRankings(rankings : Ranking[]): Ranking[] {
+    const total = rankings.length;
+    const player_rankings_ng = rankings.filter( r => r.playerId = rankings[0].playerId).length;
+    console.log("*** slicing"+ rankings + "\t selecting:" + total/player_rankings_ng);
+    return rankings.slice(0, total/player_rankings_ng);
+  }
   // Feature stop: Conflict with bga premium elo chart
   // getELOs(playerId: number, gameId: number): Observable<string> {
   //   return this._http.get<string>(`${this.SITE_URL}?game${gameId}?player=${playerId}`);
--- src/app/player-rankingtable/player-rankingtable.component.ts
+++ src/app/player-rankingtable/player-rankingtable.component.ts
@@ -1,8 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
 import {Player} from "../core/models/Player";
 import {Game} from "../core/models/Game";
 import {RankingsService} from "../core/services/rankings.service";
 import {Ranking} from "../core/models/Ranking";
+import {groupBy, mergeMap, toArray} from "rxjs/operators";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-player-rankingtable',
@@ -19,18 +21,53 @@ export class PlayerRankingtableComponent implements OnInit {
   @Input() game: Game;
 
   ngOnInit(): void {
-    this.getRankings()
+    this.getRankings(this.game.id)
   }
 
-  getRankings() {
+  ngOnChanges(changes: SimpleChanges) {
+
+    this.getRankings(changes.game.currentValue.id);
+
+  }
+
+  getRankings(gameId: string) {
     let endDate = new Date();
     let startDate = new Date(new Date().setDate(new Date().getDate() - 1)); // Yesterday
     // let end = new Date(new Date().setDate(new Date().getDate() - 1)); // Yesterday
     console.log("-------------------------------");
     console.log(startDate);
     console.log(endDate);
-    this.rankingService.getRankingsBetween(this.player.id, startDate, endDate).subscribe(rankinks =>
-      this.rankings = rankinks
+    this.rankingService.getRankingsBetween(this.game.id, startDate, endDate).subscribe(rankinks => {
+        this.rankings = this._slicePlayerRankings(this._sliceRankingsOccurences(rankinks), this.player.id, 10);
+
+      }
     );
   }
+
+  private _sliceRankingsOccurences(rankings: Ranking[]): Ranking[] {
+    const total = rankings.length;
+    const player_rankings_ng = rankings.filter(r => r.playerId == rankings[0].playerId).length;
+    console.log("*** slicing (total:" + total + ", nb" + player_rankings_ng + "\t selecting:" + total / player_rankings_ng);
+    return rankings.slice(0, total / player_rankings_ng);
+  }
+
+  private _slicePlayerRankings(rankings: Ranking[], playerId: string, count: number): Ranking[] {
+    const total = rankings.length;
+    const playerRank = rankings.filter(r => r.playerId == playerId)[0].rank;
+
+    return rankings.filter(ranking => Math.abs(ranking.rank - playerRank) <= count / 2);
+  }
+
+  // groupBy(objectArray) {
+  //   return objectArray.reduce((acc, obj) => {
+  //     const key = obj["playerId"];
+  //     if (!acc[key]) {
+  //       acc[key] = [];
+  //     }
+  //     // Add object to list for given key's value
+  //     acc[key].push(obj);
+  //     return acc;
+  //   }, {});
+  // }
+
 }
